0dc630794146290f496ae9c79ab331fe0ed95968
Vlad Gogov <gogov.vladd@gmail.com>
2021-04-25 12:00:55 +0300

    Update cellular_state_application.h

--- modules/30_rule/include/cellular_state_application.h
+++ modules/30_rule/include/cellular_state_application.h
@@ -8,9 +8,9 @@
 class CellularStateApplication {
     std::string help(const std::string& filename) const;
 
-public:
+ public:
     CellularStateApplication() = default;
     std::string operator()(int argc, const char* argv[]) const;
 };
 
-#endif  // MODULES_30_RULE_INCLUDE_CELL_CELLULAR_APPLICATION_H_
+#endif  // MODULES_30_RULE_INCLUDE_CELLULAR_STATE_APPLICATION_H_
